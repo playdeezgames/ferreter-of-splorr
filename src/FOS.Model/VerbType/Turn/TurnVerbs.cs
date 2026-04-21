3e652ce79c713f677a8a0d5e35e18d15984740ee
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-04-21 04:45:15 -0500

    turn verbs

--- src/FOS.Model/VerbType/Turn/TurnVerbs.cs
+++ src/FOS.Model/VerbType/Turn/TurnVerbs.cs
@@ -8,9 +8,36 @@
         internal readonly static string TURN_AROUND = GetName(nameof(TURN_AROUND));
         internal static readonly IReadOnlyList<IVerbType> All =
             [
-                new TurnLeftVerbType(),
-                new TurnRightVerbType(),
-                new TurnAroundVerbType()
+                new VerbType(
+                    TURN_LEFT,
+                    (x,_) => !x.IsDead() && x.HasMode() && x.GetMode() == Modes.TURN,
+                    x=> "Turn Left",
+                    (x,_) =>
+                    {
+                        x.Direction = Directions.All[x.Direction].Previous;
+                        x.ClearMode();
+                        x.AddMessage(Moods.NORMAL, "You turn left.");
+                    }),
+                new VerbType(
+                    TURN_RIGHT,
+                    (x,_) => !x.IsDead() && x.HasMode() && x.GetMode() == Modes.TURN,
+                    x=> "Turn Right",
+                    (x,_) =>
+                    {
+                        x.Direction = Directions.All[x.Direction].Next;
+                        x.ClearMode();
+                        x.AddMessage(Moods.NORMAL, "You turn right.");
+                    }),
+                new VerbType(
+                    TURN_AROUND,
+                    (x,_) => !x.IsDead() && x.HasMode() && x.GetMode() == Modes.TURN,
+                    x=> "Turn Around",
+                    (x,_) =>
+                    {
+                        x.Direction = Directions.All[x.Direction].Opposite;
+                        x.ClearMode();
+                        x.AddMessage(Moods.NORMAL, "You turn around.");
+                    }),
             ];
     }
 }
