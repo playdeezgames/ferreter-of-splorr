f817f892d501b34408cfbee59859e0e62468fae7
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-04-21 05:29:24 -0500

    refactored choices

--- src/FOS.Model/Dialog/N00bChoices.cs
+++ src/FOS.Model/Dialog/N00bChoices.cs
@@ -5,72 +5,75 @@ namespace FOS.Model.Dialog
 {
     internal static class N00bChoices
     {
+        private record ChoiceGenerator(Func<ICharacter, bool> Condition, Func<ICharacter, IEnumerable<IDialogChoice>> ChoiceSource);
+
+        private readonly static IEnumerable<ChoiceGenerator> choiceGenerators =
+            [
+                new ChoiceGenerator(
+                    x=>
+                        x.HasMode() &&
+                        x.GetMode() == Modes.FEATURES &&
+                        !x.HasFocusFeature,
+                    x=>
+                        x.Location.
+                            Features.
+                            Select(x =>
+                                new DialogChoice(
+                                    [x.FeatureId.ToString()],
+                                    x.Name))),
+                new ChoiceGenerator(
+                    x=>
+                        x.HasMode() &&
+                        x.GetMode() == Modes.INVENTORY &&
+                        !x.HasFocusItem,
+                    x=>
+                        x.Inventory.
+                            Items.
+                            Select(x =>
+                                new DialogChoice(
+                                    [x.ItemId.ToString()],
+                                    x.Name))),
+                new ChoiceGenerator(
+                    x=>
+                        x.HasMode() &&
+                        x.GetMode() == Modes.GROUND_INVENTORY &&
+                        !x.HasFocusItem,
+                    x=>
+                        x.Location.
+                            Inventory.
+                            Items.
+                            Select(x =>
+                                new DialogChoice(
+                                    [x.ItemId.ToString()],
+                                    x.Name))),
+                new ChoiceGenerator(
+                    x=>
+                        x.HasMode() &&
+                        x.GetMode() == Modes.CHARACTERS &&
+                        !x.HasFocusCharacter,
+                    x=>
+                        x.Location.
+                            GetOtherCharacters(x).
+                            Select(y =>
+                                new DialogChoice(
+                                    [y.CharacterId.ToString()],
+                                    y.Name))),
+                new ChoiceGenerator(
+                    x=> true,
+                    x=>
+                        Verbs.All.
+                            Where(y =>
+                                y.Value.
+                                    CanPerform(x)).
+                                    Select(z =>
+                                        new DialogChoice(
+                                            [z.Key],
+                                            z.Value.GetText(x))))
+            ];
+
         internal static IEnumerable<IDialogChoice> GetChoices(ICharacter character)
         {
-            return [
-                    .. AddFeaturesChoices(character),
-                    .. AddInventoryChoices(character),
-                    .. AddGroundInventoryChoices(character),
-                    .. AddCharactersChoices(character),
-                    .. AddVerbChoices(character)
-                ];
-        }
-        private static IEnumerable<IDialogChoice> AddGroundInventoryChoices(ICharacter character)
-        {
-            if (
-                !character.HasMode() ||
-                character.GetMode() != Modes.GROUND_INVENTORY ||
-                character.HasFocusItem)
-            {
-                return [];
-            }
-            return character.Location.Inventory.Items.Select(x => new DialogChoice([x.ItemId.ToString()], x.Name));
-        }
-
-        private static IEnumerable<IDialogChoice> AddInventoryChoices(ICharacter character)
-        {
-            if (
-                !character.HasMode() ||
-                character.GetMode() != Modes.INVENTORY ||
-                character.HasFocusItem)
-            {
-                return [];
-            }
-            return character.Inventory.Items.Select(x => new DialogChoice([x.ItemId.ToString()], x.Name));
-        }
-
-        private static IEnumerable<IDialogChoice> AddVerbChoices(ICharacter character)
-        {
-            return Verbs.All.
-                Where(x => x.Value.CanPerform(character)).
-                Select(x => new DialogChoice([x.Key], x.Value.GetText(character)));
-        }
-
-        private static IEnumerable<IDialogChoice> AddFeaturesChoices(ICharacter character)
-        {
-            if (
-                !character.HasMode() ||
-                character.GetMode() != Modes.FEATURES ||
-                character.HasFocusFeature)
-            {
-                return [];
-            }
-            return character.Location.Features.Select(x => new DialogChoice([x.FeatureId.ToString()], x.Name));
-        }
-
-        private static IEnumerable<IDialogChoice> AddCharactersChoices(ICharacter character)
-        {
-            if (
-                !character.HasMode() ||
-                character.GetMode() != Modes.CHARACTERS ||
-                character.HasFocusCharacter)
-            {
-                return [];
-            }
-            return character.
-                Location.
-                GetOtherCharacters(character).
-                Select(x => new DialogChoice([x.CharacterId.ToString()], x.Name));
+            return choiceGenerators.Where(y => y.Condition(character)).Select(z => z.ChoiceSource(character)).SelectMany(x => x);
         }
     }
 }
