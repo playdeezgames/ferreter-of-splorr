7364f19ddb561d22d4274753b4bd23a4794c1515
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-04-21 04:07:06 -0500

    feature verbs have been made into verbtypes

--- src/FOS.Model/VerbType/Feature/FeatureVerbs.cs
+++ src/FOS.Model/VerbType/Feature/FeatureVerbs.cs
@@ -8,9 +8,21 @@
         internal readonly static string EXAMINE_FEATURE = GetName(nameof(EXAMINE_FEATURE));
         internal static readonly IReadOnlyList<IVerbType> All =
             [
-                new SearchFeatureVerbType(),
-                new ExamineFeatureVerbType(),
-                new BlurFeatureVerbType()
+                new VerbType(
+                    SEARCH_FEATURE,
+                    (x, _) => !x.IsDead() && (x.FocusFeature?.HasTrigger(Triggers.SEARCH) ?? false),
+                    x=> "Search...",
+                    (x, _)=> x.FocusFeature!.FireTrigger(Triggers.SEARCH, x)),
+                new VerbType(
+                    EXAMINE_FEATURE,
+                    (x, _) => !x.IsDead() && (x.FocusFeature?.HasTrigger(Triggers.EXAMINE) ?? false),
+                    x=>"Examine...",
+                    (x, _)=> x.FocusFeature!.FireTrigger(Triggers.EXAMINE, x)),
+                new VerbType(
+                    BLUR_FEATURE,
+                    (x, _) => !x.IsDead() && x.HasFocusFeature,
+                    x=> "Features...",
+                    (x, _)=> x.FocusFeature = null),
             ];
     }
 }
